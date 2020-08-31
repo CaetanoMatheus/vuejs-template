bc08e424a8bdd36b971096530ef9c04188fa4732
Matheus Caetano <caetanomatheus876@gmail.com>
2020-08-30 21:18:14 -0300

    Template

---
+++ src/components/Text/Title/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="title">
+    <h1 class="title__mainTitle">{{ title }}</h1>
+    <h5 class="title__subtext">{{ subText }}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    subText: {
+      type: String,
+      required: false,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+  &__mainTitle {
+    color: $white;
+  }
+
+  &__subtext {
+    color: $light-grey;
+  }
+}
+</style>
---
+++ src/pages/Home/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="helloWold">
+    <Title title="Hello World!!" subText="It's time to code." />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    Title: () => import('@/components/Text/Title'),
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.helloWold {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: $purple;
+}
+</style>
