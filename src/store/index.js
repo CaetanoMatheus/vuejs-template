84430d4ec2da4963c177268dde6e4495d9791dd5
Matheus Caetano <caetanomatheus876@gmail.com>
2020-09-07 11:11:36 -0300

    Template

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import actions from './actions'
+import getters from './getters'
+import mutations from './mutations'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+
+  modules: {},
+
+  actions,
+
+  getters,
+
+  mutations,
 })
